643f7ff83680afc019af33a8074db273af1409e2
Percepteme <57575981+Percepteme@users.noreply.github.com>
2022-03-31 13:22:46 +0100

    Update ROI-acquisitor.py
    Description of code

--- ImageJ macros/ROI-acquisitor.py
+++ ImageJ macros/ROI-acquisitor.py
@@ -1,3 +1,8 @@
+
+#Source: https://cellpose.readthedocs.io/en/latest/outputs.html [accessed March 2021]
+
+####### ImageJ macro designed to map ROIs onto loaded image files using the cellpose cp_masks_outlines.txt file #### 
+## See test data for examples of the output format ##
 from ij import IJ
 from ij.plugin.frame import RoiManager
 from ij.gui import PolygonRoi
@@ -9,21 +14,21 @@ fd.show()
 file_name = fd.getDirectory() + fd.getFile()
 print(file_name)
 
-RM = RoiManager()
-rm = RM.getRoiManager()
+RM = RoiManager() ## ROI manager object wrapper
+rm = RM.getRoiManager() ## select ROI manager
 
-imp = IJ.getImage()
+imp = IJ.getImage() ## get imaged currently loaded in ImageJ
 
-textfile = open(file_name, "r")
+textfile = open(file_name, "r") ## open the txt file
 for line in textfile:
-    xy = map(int, line.rstrip().split(","))
-    X = xy[::2]
+    xy = map(int, line.rstrip().split(",")) # for each pair of lines in the text file, extract the coordinate data of the masks
+    X = xy[::2] ## select every other even line as x-coordinates 
     Y = xy[1::2]
     imp.setRoi(PolygonRoi(X, Y, Roi.POLYGON))
     # IJ.run(imp, "Convex Hull", "")
-    roi = imp.getRoi()
+    roi = imp.getRoi() 
     print(roi)
-    rm.addRoi(roi)
+    rm.addRoi(roi) ## add ROI to the ROI manager
 textfile.close()
 rm.runCommand("Associate", "true")
-rm.runCommand("Show All with labels")
+rm.runCommand("Show All with labels") ## show with labels 
